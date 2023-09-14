493ee030a05c4978590560928205130072caaf69
developer-vsandhu <77821789+developervinay1@users.noreply.github.com>
2023-09-15 02:43:15 +0530

    Movies Rows And Sections Completed

---
+++ components/MoviesRows.js
@@ -0,0 +1,17 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+import moviesURL from "../data/moviesURL";
+import MovieRow from "./MovieRow";
+const MovieRows = () => {
+  const data = moviesURL;
+  return (
+    <View>
+      {data.map((movie, index) => (
+        <MovieRow key={index} title={movie.name} url={movie.url} />
+      ))}
+      <Text>MovieRows</Text>
+    </View>
+  );
+};
+
+export default MovieRows;
